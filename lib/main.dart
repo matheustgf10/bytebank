8e86cd81ea78d034b4cb356cd30368a549b5ee11
Hebert Júlio <hebertjulio@users.noreply.github.com>
2020-09-21 08:10:45 -0300

    Adicionando componentens do Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(
-    children: <Widget>[
-      Text(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
       ),
-      Text(
-        'Curabitur laoreet mollis sagittis.',
-        textDirection: TextDirection.ltr,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
       ),
-      Text(
-        'Mauris dictum fringilla aliquam.',
-        textDirection: TextDirection.ltr,
-      ),
-      Column(
-        children: <Widget>[
-          Text(
-            'Nulla posuere dictum mi, eget vehicula ex ullamcorper sit amet.',
-            textDirection: TextDirection.ltr,
-          ),
-        ],
-      ),
-    ],
+    ),
   ));
 }
